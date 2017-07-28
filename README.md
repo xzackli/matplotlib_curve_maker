78e4fd0b6c1fa456be34bf44d9e3bac5c0313b78
Zack Li <xzackli@gmail.com>
2017-07-28 16:22:26 -0700

    added some scripts I was working on to this repo. I found myself needing something like this for three unrelated projects. probably useful to have a general, easy to modify tool

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # matplotlib_curve_maker
-sometimes you just gotta plot some stuff, and then 
+
+> 70\% done, not totally functional yet
+
+This is pretty much for personal use for when I'm thinking about simple numerical models. The dream is to be plotting something, i.e.
+
+```python
+import numpy as np
+import matplotlib.pyplot as plot
+
+x = np.linspace(-np.pi, np.pi)
+y = np.sin(x)
+
+plt.plot(x,y)
+```
+![plot of a sine curve](readme_images/just_a_plot.png)
+
+You run this through your model. Suddenly, you get an idea.
+
+> What does your model say, if this function starts declining on the right side?
+
+So you import this package, run some stuff.
+
+```
+import matplotlib_curve_maker
+
+ax = plt.gca()
+points, = ax.plot([], [], 'o', c=color_cycle[1], markersize=6) # empty points
+line, = ax.plot([], [], '-' , c=color_cycle[1] ) # empty line
+linebuilder = LineBuilder(ax, points, line)
+
+```
+
+A window pops up. You have some keyboard controls.
+* `i` to add a point where the mouse is.
+* `d` to delete the point under the mouse.
+* Click and drag points to move them around.
+![spline in there](readme_images/spline_in_there.png)
---
+++ curve_maker.py
@@ -0,0 +1,157 @@
+
+
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
+
+color_cycle = [s['color'] for s in list(matplotlib.rcParams['axes.prop_cycle'])]
+
+# this script lets you plot points, and produces a spline fit for you.
+
+class LineBuilder:
+    def __init__(self, ax, points, line, input_disable_key='1', activated=True):
+        self.ax = ax
+        self.line = line
+        self.points = points
+        self.xs = list(points.get_xdata())
+        self.ys = list(points.get_ydata())
+        self._ind = None
+        self.epsilon = 12
+        self.input_disable_key = input_disable_key
+        self.keyboard_activated = activated
+#         self.cid = points.figure.canvas.mpl_connect('button_press_event', self)
+
+        points.figure.canvas.mpl_connect('button_press_event', self.button_press_callback)
+        points.figure.canvas.mpl_connect('button_release_event', self.button_release_callback)
+        points.figure.canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
+        points.figure.canvas.mpl_connect('key_press_event', self.key_press_callback)
+
+    def get_ind_under_point(self, event):
+        'get the index of the vertex under point if within epsilon tolerance'
+
+        # avoid deleting when nothing is there
+        if len(self.xs) == 0:
+            return None
+
+
+        # display coords
+        xy = np.transpose( np.array([self.xs,self.ys]) )
+        xyt = self.points.get_transform().transform(xy)
+
+        xt, yt = xyt[:, 0], xyt[:, 1]
+        d = np.sqrt((xt - event.x)**2 + (yt - event.y)**2)
+        indseq = np.nonzero(np.equal(d, np.amin(d)))[0]
+        ind = indseq[0]
+
+        if d[ind] >= self.epsilon:
+            ind = None
+
+        return ind
+
+    def update_points(self):
+        self.points.set_data(self.xs, self.ys)
+
+        if len(self.xs) > 3:
+            f = interp1d(self.xs, self.ys, kind='cubic')
+            xnew = np.linspace(np.min(self.xs), np.max(self.xs), \
+                               num=101, endpoint=True)
+            self.line.set_data(xnew, f(xnew))
+        else:
+            self.line.set_data([], [])
+
+
+    def key_press_callback(self, event):
+        'whenever a key is pressed'
+        if event.key == self.input_disable_key:
+            self.keyboard_activated = not self.keyboard_activated
+            if self.keyboard_activated:
+                self.points.set_alpha(1.0)
+            else:
+                self.points.set_alpha(0.1)
+
+        if not self.keyboard_activated:
+            return
+        if not event.inaxes:
+            return
+        if event.key == ' ':
+
+            print('self.xs = ', [xx-1 for xx in linebuilder.xs])
+            print('self.ys = ', linebuilder.ys)
+            pass
+        elif event.key == 'd':
+            ind = self.get_ind_under_point(event)
+            if ind != None:
+                del(self.xs[ind])
+                del(self.ys[ind])
+                self.update_points()
+        elif event.key == 'i':
+
+            if event.inaxes!=self.points.axes: return
+            self.xs.append(event.xdata)
+            self.ys.append(event.ydata)
+            self.update_points()
+
+
+        self.points.figure.canvas.draw()
+
+    def button_press_callback(self, event):
+        'whenever a mouse button is pressed'
+
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+        self._ind = self.get_ind_under_point(event)
+
+    def button_release_callback(self, event):
+        'whenever a mouse button is released'
+
+        if event.button != 1:
+            return
+        self._ind = None
+
+    def motion_notify_callback(self, event):
+        'on mouse movement'
+
+        if self._ind is None:
+            return
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+        x, y = event.xdata, event.ydata
+
+        self.xs[self._ind],self.ys[self._ind] = x, y
+        self.update_points()
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+
+# this is the place where you put some custom plotting routines
+
+
+
+f, [ax, ax_aux] = plt.subplots(2, sharex=True)
+ax.set_xlabel('1+z')
+ax.set_xlim(1,7)
+ax.set_ylim(1e-3,1)
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+z = np.linspace(0,6,100)
+# ax.plot( z+1, sfr, label=r'SFR' )
+psi =  0.268 *(1. + 0.75*z)**6.81 * np.exp(-z**0.922 / 0.302) + 0.032
+ax.plot(z+1,psi, '--', label=r'Scaled Madau')
+
+
+ax.set_title('click to build a spline')
+points, = ax.plot([], [], 'o', c=color_cycle[1], markersize=6)  # empty points
+line, = ax.plot([], [], '-' , c=color_cycle[1] ) # empty line
+linebuilder = LineBuilder(ax, points, line, '1', True)
+
+plt.legend()
+plt.show()
+
+# print('done!')
